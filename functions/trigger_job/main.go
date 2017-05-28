0812219294b823c139ed446e6f8ce2a03edf8e7c
Michiaki Mizoguchi <michiaki.mizoguchi@gmail.com>
2017-05-28 09:02:42 +0900

    Fix request

--- functions/trigger_job/main.go
+++ functions/trigger_job/main.go
@@ -5,33 +5,46 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
-	"net/url"
 	"os"
 
 	"github.com/apex/go-apex"
 )
 
+type payload struct {
+	PipelineId string `json:"pipelineId"`
+	Message    string `json:"message"`
+}
+
 func main() {
 	apex.HandleFunc(func(event json.RawMessage, ctx *apex.Context) (interface{}, error) {
 		client := &http.Client{}
+
+		p := payload{
+			PipelineId: os.Getenv("WERCKER_PIPELINE_ID"),
+			Message:    os.Getenv("WERCKER_MESSAGE"),
+		}
+		b, _ := json.Marshal(p)
+
 		var buffer bytes.Buffer
-		var query = fmt.Sprintf("pipelineId=%v&message=%v", os.Getenv("WERCKER_PIPELINE_ID"), url.QueryEscape(os.Getenv("WERCKER_MESSAGE")))
-		buffer.WriteString(query)
+		buffer.WriteString(string(b))
 
 		request, err := http.NewRequest("POST", "https://app.wercker.com/api/v3/runs/", &buffer)
 		request.Header.Add("Authorization", fmt.Sprintf("Bearer %v", os.Getenv("WERCKER_TOKEN")))
-		request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+		request.Header.Add("Content-Type", "application/json")
 
 		resp, err := client.Do(request)
 		if err != nil {
-			return nil, err
+			log.Fatal(err)
 		}
+
 		defer resp.Body.Close()
 		body, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
-			return nil, err
+			log.Fatal(err)
 		}
+
 		return string(body), nil
 	})
 }
